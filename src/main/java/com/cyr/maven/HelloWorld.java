521ec37ef109204f80de925688ea4c965ef8749d
chenyarulucky <chenyaru@163.com>
2021-03-08 19:28:54 +0800

    init git-01-first

---
+++ src/main/java/com/cyr/maven/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.cyr.maven;
+
+public class HelloWorld {
+    public static void main(String[] agrs) {
+        System.out.println("Hello Maven!");
+        try {
+            System.out.println("123");
+            System.out.println("123");
+            System.out.println("123");
+
+            System.out.println("12sd3");
+            System.out.println("1sds23");
+            System.out.println("12sdsd3");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void getGrade(String shopNick, String buyernick) {
+        /**
+         * @TODO
+         * @Date 2021/3/4
+         * @Author ParadiseTricia
+         * @param  [shopNick, buyernick]
+         * @return void
+         */
+        String str = "";
+
+
+    }
+}
